fff7778966b535c0d81b0597a5af619649bd5b82
duncan <dwerner@riskamp.com>
2016-06-22 16:16:54 -0700

    scrub main

--- main/main.js
+++ main/main.js
@@ -1,15 +1,10 @@
 'use strict';
 
-// installer 
-if (require('electron-squirrel-startup')) return;
+if (require('electron-squirrel-startup')) return; // installer
 
 const path = require( "path" );
 const electron = require('electron');
 const electronwindowstate = require('electron-window-state');
-
-// const ipcMain = require('electron').ipcMain;
-// const app = electron.app;
-// const BrowserWindow = electron.BrowserWindow;
 
 const {ipcMain} = electron;
 const {app} = electron;
@@ -17,35 +12,57 @@ const {BrowserWindow} = electron;
 
 let win;
 
-// handle command line arguments
-//
-// --r-home=X 
-//
+app.on('ready', function(){
 
-/** 
- * this moved to renderer, at least for now; if you need 
- * to process args in main just turn it back on
- *
-process.argv.forEach( function( arg ){
-	var m = arg.match( /--r-home=(.*?)$/ );
-	if( m ){
-		var p = path.resolve(untildify(m[1]));
-		console.info( "P", p );
-		process.env.R_HOME = p;
-	}
-	else console.info( "nope", arg );
+	let windowstate = electronwindowstate({
+		defaultWidth: 1000,
+		defaultHeight: 800
+	});
+	
+	win = new BrowserWindow({
+		'x': windowstate.x,
+		'y': windowstate.y,
+		'width': windowstate.width,
+		'height': windowstate.height,
+		'webPreferences': {
+			'experimentalCanvasFeatures': true
+		}
+	});
+	
+	win.main_process_args = process.argv.slice(0);
+	
+    windowstate.manage(win);
+    win.loadURL('file://' + __dirname + '/index.html');
+    win.on('closed', function() { win = null; });
+
 });
-*/
 
-var download = function(win, opts = {}){
-	
-	var url = opts.url;
-	var dest = opts.destfile;
-	
-	var listener = function(event, item, webContents){
+app.on('window-all-closed', function () {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', function () {
+	if (win === null) {
+		createWindow();
+	}
+});
+
+ipcMain.on('system', function(event, arg){
+	if( arg === "quit" ){
+		app.quit();
+	}
+});
+
+ipcMain.on('download', function(event, opts = {}){
+
+	let url = opts.url;
+	let dest = opts.destfile;
+	let listener = function(event, item, webContents){
 		
-		var totalBytes = item.getTotalBytes();
-		var filePath = dest || path.join(app.getPath('downloads'), item.getFilename());
+		let totalBytes = item.getTotalBytes();
+		let filePath = dest || path.join(app.getPath('downloads'), item.getFilename());
 
 		// NOTE: this fails with unix-y paths.  R is building these paths incorrectly
 
@@ -79,59 +96,4 @@ var download = function(win, opts = {}){
 	win.webContents.session.on( 'will-download', listener );
 	win.webContents.downloadURL(url);
 	
-};
-
-var createWindow = function(){
-
-	let windowstate = electronwindowstate({
-		defaultWidth: 1000,
-		defaultHeight: 800
-	});
-	
-	// Create the browser window.
-	win = new BrowserWindow({
-		'x': windowstate.x,
-		'y': windowstate.y,
-		'width': windowstate.width,
-		'height': windowstate.height,
-		'webPreferences': {
-			'experimentalCanvasFeatures': true
-		}
-	});
-	
-	win.main_process_args = process.argv.slice(0);
-	
-    windowstate.manage(win);
-    win.loadURL('file://' + __dirname + '/index.html');
-
-    win.on('closed', function() {
-        win = null;
-    });
-    
-}
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', function () {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
 });
-
-app.on('activate', function () {
-	if (win === null) {
-		createWindow();
-	}
-});
-
-ipcMain.on('system', function(event, arg){
-	if( arg === "quit" ){
-		app.quit();
-	}
-});
-
-ipcMain.on('download', function(event, arg){
-	// console.info( arg );
-	download( win, arg );
-});
-
