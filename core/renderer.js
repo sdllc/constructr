54f3b7d21324d8d8e292ff03f465092e8814ec6b
duncan <dwerner@riskamp.com>
2016-07-14 10:45:37 -0700

    sort locals

--- core/renderer.js
+++ core/renderer.js
@@ -727,7 +727,10 @@ var open_locals = function(){
         panel.header.addEventListener( "close", closelistener );
         panel.appendChild( panel.header );
 
-        let keys, data;
+        let keys, data, sortindex = -1, sortorder = 1;
+        let indexes = null;
+        let table_data; // cached for sorting
+
         let click = function(e){
 
             e.stopPropagation();
@@ -735,6 +738,14 @@ var open_locals = function(){
 
             let row = e.target.row;
             if( typeof row === "undefined" ) return;
+
+            // not on headers (doubleclick -> sort)
+            if( e.target.className.match( /header/ )) return;
+
+            if( indexes ){
+                console.info( row, "->", indexes[row] );
+                row = indexes[row];
+            }
 
             let key = keys[row];
             let val = data[key];
@@ -759,7 +770,25 @@ var open_locals = function(){
 
         };
 
+        let doubleclick = function(e){
+
+            e.stopPropagation();
+            e.preventDefault();
+
+            if( !e.target.className.match( /header/ )) return;
+
+            let col = e.target.col;
+            if( sortindex === col ) sortorder = -sortorder;
+            else {
+                sortindex = col;
+                sortorder = 1;
+            }
+            push_data( true );
+
+        };
+
         panel.addEventListener( "click", click );
+        panel.addEventListener( "dblclick", doubleclick );
 
         let context = function(e){
             e.stopPropagation();
@@ -807,9 +836,60 @@ var open_locals = function(){
         panel._onHide = function(){ delete( locals_registry.locals ); };
 		panel._onShow = function(){ locals_registry.locals = 1; };
 		panel._onUnload = function(){ 
+            panel.removeEventListener( "click", click );
+            panel.removeEventListener( "dblclick", doubleclick );
+            panel.removeEventListener( "contextmenu", context );
+
             PubSub.unsubscribe( token );
             delete( locals_registry.locals ); 
         };
+
+        let push_data = function( resort ){
+
+            if( resort && sortindex >= 0 && keys.length > 0 ){
+
+                // rank then sort, optionally reverse
+                let x = new Array( keys.length );
+                for( let i = 0; i< keys.length; i++ ){
+                    x[i] = [ table_data[sortindex][i], i ];
+                }
+                x.sort(function(a, b) {
+                    return +(a[0] > b[0]) || +(a[0] === b[0]) - 1;
+                });
+                if( sortorder < 0 ) x.reverse();
+
+                // now reorder data
+                let tmp = new Array(table_data.length);
+                for( let j = 0; j< table_data.length; j++ ){
+                    tmp[j] = new Array( keys.length );
+                }
+
+                // faster way?
+                for( let j = 0; j< table_data.length; j++ ){
+                    for( let i = 0; i< keys.length; i++ ){
+                        tmp[j][i] = table_data[j][x[i][1]];
+                    }
+                }
+
+                // set
+                table_data = tmp;
+
+                // resort keys for click map
+                tmp = new Array( keys.length );
+                for( let i = 0; i< keys.length; i++ ) tmp[i] = keys[x[i][1]];
+                keys = tmp;
+
+            }
+            else indexes = null;
+
+            panel.node.update({
+                fixed: true,
+                data: table_data,
+                column_classes: [ "string", "string", "left" ],
+                column_headers: [ Messages.FIELD, Messages.CLASS, Messages.VALUE ]
+            })
+
+        }
 
         let on_locals = function(msg, locals){
 		
@@ -821,8 +901,8 @@ var open_locals = function(){
 			
 			keys = locals.$data.fields && locals.$data.fields.$names ? locals.$data.fields.$names : [];
 			data = locals.$data.fields && locals.$data.fields.$data ? locals.$data.fields.$data : {};
+            table_data = [ keys, Array(keys.length), Array(keys.length) ];
 
-            let table_data = [ keys, Array(keys.length), Array(keys.length) ];
             for( let i = 0; i< keys.length; i++ ){
                 let key = keys[i];
     		    let val = data[key];
@@ -834,12 +914,7 @@ var open_locals = function(){
                 table_data[2][i] = text;
             };
 
-            panel.node.update({
-                fixed: true,
-                data: table_data,
-                column_classes: [ "string", "string", "left" ],
-                column_headers: [ Messages.FIELD, Messages.CLASS, Messages.VALUE ]
-            })
+            push_data( true );
 
 		};
 
